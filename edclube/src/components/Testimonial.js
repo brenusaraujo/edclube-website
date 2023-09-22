ee5cffd9954d6a1ed812316597c5e3ee9126ad0a
Breno Araújo de Matos <brenoaraujodematos@MacBook-Pro-de-Breno.local>
2023-09-22 08:50:43 -0300

    adiciona sessão com a imagem

--- edclube/src/components/Testimonial.js
+++ edclube/src/components/Testimonial.js
@@ -1,33 +1,11 @@
 import React from "react";
-import ProfilePic from "../Assets/john-doe-image.png";
-import { AiFillStar } from "react-icons/ai";
 
 const Testimonial = () => {
   return (
     <div className="work-section-wrapper">
-      <div className="work-section-top">
-        <p className="primary-subheading">Testemunhos</p>
-        <h1 className="primary-heading">O que estão falando</h1>
-        <p className="primary-text">
-          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
-          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
-        </p>
-      </div>
-      <div className="testimonial-section-bottom">
-        <img src={ProfilePic} alt="" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
-          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
-        </p>
-        <div className="testimonials-stars-container">
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-        </div>
-        <h2>Joao Nome teste</h2>
-      </div>
+      <h1>
+        Com nossa assistência 24 horas, você nunca estará sozinho na estrada!
+      </h1>
     </div>
   );
 };
